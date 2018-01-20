c94f8ec62d8cb319f49d5c083a109e0dfdef7950
Fedor Tsyganov <fedortnn@gmail.com>
2018-01-20 12:20:38 -0800

    update

--- src/test/java/constants/Const.java
+++ src/test/java/constants/Const.java
@@ -6,23 +6,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.Set;
 
-/**
- * Created by 2012mba4gb128gb on 1/30/17.
- */
+
 public class Const {
 
     public static WebDriver webDriver;
     public static final String chromeDriver = "webdriver.chrome.driver";
-    public static final String pathToCD = "/Users/2012mba4gb128gb/Desktop/dev/chromedriver";
-    public static final String baseUrl = "https://qa-portal.clickatelllabs.com/#";
-    public static final String baseEmailNames [] = {"fedor.clickatell", "mc2testportal"};
+    public static final String pathToCD = "dev/chromedriver";
+    public static final String baseUrl = "https://link.to/app";
+    public static final String baseEmailNames [] = {"user", "testportal"};
     public static final String defaultPassword = "12345678";
     public static final String HEADER_JSON = "application/json";
 
-    public static final User firstTimeUser = new User("fedor.clickatell+0023@gmail.com","12345678",
+    public static final User firstTimeUser = new User("user@email.com","12345678",
             "Account", "B");
 
-    public static final User userWithDetails = new User("fedor.clickatell+555@gmail.com","12345678",
+    public static final User userWithDetails = new User("user@email.com+1@email.com","12345678",
             "Sergio", "Marko");
 
     public static final String leftNavNames []
